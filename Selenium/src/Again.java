5a1a08b3c135fae0440a6cde24e9444c25b717e4
enqrswn <enqrswn@IN-00232030.ericsson.se>
2022-04-11 22:12:17 +0530

    new files

---
+++ Selenium/src/Again.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Again
+{
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver","C:/Pradeep/Java/chromedriver_win32/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.navigate().to("https://www.google.com/");
+		driver.findElement(By.cssSelector("body[jsmodel='hspDDf']")).sendKeys("Pradeep");
+		
+		
+	}
+
+}
+
